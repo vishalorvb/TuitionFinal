ffbe04a1ff965558d143c607c54551b7ba61790b
vishalorvb <63139150+vishalorvb@users.noreply.github.com>
2023-07-18 21:31:45 +0530

    verify email path working

--- Tuition/usermanager/urls.py
+++ Tuition/usermanager/urls.py
@@ -9,8 +9,5 @@ urlpatterns = [
     re_path(r'^/login$', views.login_page, name='login_page'),
     re_path(r'^/verify_otp$', views.verify_otp, name='verify_otp'),
     re_path(r'^/logout', views.user_logout, name='logout'),
-    path(r'^verify_email/<str:link>', views.verify_email, name='verify_email')
-
-
-
+    re_path(r'^/verify_email/(?P<link>.+)/$', views.verify_email, name='verify_email')
 ]
